b4b1afa914d62a7a49d5b50c1ec5257e944ea27d
Ekaterina <Kata_Yurevna@mail.ru>
2022-08-31 05:03:59 +0700

    add eslint & prettier

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
 require('dotenv').config();
 const express = require('express');
-const sequelize = require('./db');
-const models = require('./models/models');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
+const path = require('path');
+const sequelize = require('./db');
+const models = require('./models/models');
 const router = require('./routes/index');
 const errorHandler = require('./middleware/ErrorHandlingMiddleware');
-const path = require('path');
-
 
 const PORT = process.env.PORT || 5000;
 
@@ -21,15 +20,14 @@ app.use('/api', router);
 // Обработка ошибок, последний Middleware
 app.use(errorHandler);
 
-const start = async ()=> {
-    try{
-        await sequelize.authenticate();
-        await sequelize.sync();
-        app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`))
-    }catch(e){
-        console.log(e);
-    }
-}
+const start = async () => {
+	try {
+		await sequelize.authenticate();
+		await sequelize.sync();
+		app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+	} catch (e) {
+		console.log(e);
+	}
+};
 
 start();
-
