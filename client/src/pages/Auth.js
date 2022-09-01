fda19e78ef4f4f731d49a9ae41147a877a1d2693
Ekaterina <Kata_Yurevna@mail.ru>
2022-09-01 21:33:05 +0700

    add page auth & registration

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,10 +1,55 @@
 import React from 'react';
+import { Container, Form } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import { NavLink, useLocation } from "react-router-dom";
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from "../utils/consts";
 
 const Auth = () => {
+    const location = useLocation();
+     const isLogin = location.pathname === LOGIN_ROUTE;
     return (
-        <div>
-            AUTH
-        </div>
+        <Container
+            className="d-flex justify-content-center align-items-center"
+            style={{height: window.innerHeight - 54}}
+        >
+            <Card style={{width: 600}} className="p-5">
+                <h2 className="m-auto">{isLogin ? 'Авторизация' : "Регистрация"}</h2>
+                <Form className="d-flex flex-column">
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введите ваш email..."
+                        value="email"
+                        
+                    />
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введите ваш пароль..."
+                        value="password"
+                        type="password"
+                    />
+                    <Row className="d-flex justify-content-between mt-3 pl-3 pr-3">
+                        {isLogin ?
+                            <div>
+                                Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся!</NavLink>
+                            </div>
+                            :
+                            <div>
+                                Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войдите!</NavLink>
+                            </div>
+                        }
+                        <Button
+                            variant={"outline-success"}
+                            onClick={()=> console.log('click')}
+                        >
+                            {isLogin ? 'Войти' : 'Регистрация'}
+                        </Button>
+                    </Row>
+
+                </Form>
+            </Card>
+        </Container>
     );
 };
 
