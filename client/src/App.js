d1b8f1bd2d676c0e30f056cd2234ee181adfd97a
Ekaterina <Kata_Yurevna@mail.ru>
2022-09-04 21:08:39 +0700

    update App

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import { observer } from 'mobx-react-lite';
+import React, { useContext, useEffect, useState } from 'react';
+import { Spinner } from 'react-bootstrap';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import AppRouter from './components/AppRouter/AppRouter';
 import NavBar from './components/NavBar/NavBar';
+import { Context } from './context/Context';
+import { check } from './http/userAPI';
 
-function App() {
+const App = observer(() => {
+  const { user } = useContext(Context);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    check().then((data) => {
+      user.setUser(true);
+      user.setIsAuth(true);
+    }).finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return <Spinner animation="grow" />;
+  }
   return (
     <BrowserRouter>
       <NavBar />
       <AppRouter />
     </BrowserRouter>
   );
-}
+});
 
 export default App;
