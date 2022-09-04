ea57d48d2521222f18a76a708fa06f40161a1fcb
Ekaterina <Kata_Yurevna@mail.ru>
2022-09-04 21:04:03 +0700

    update "DeviceStore"

--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -2,20 +2,14 @@ import { makeAutoObservable } from 'mobx';
 
 export default class DeviceStore {
   constructor() {
-    this._types = [
-      { id: 1, name: 'Холодильники' },
-      { id: 2, name: 'Смартфоны' },
-    ];
-    this._brands = [
-      { id: 1, name: 'Samsung' },
-      { id: 2, name: 'Apple' },
-    ];
-    this._devices = [
-      {
-        id: 1, name: '12pro', price: 100000, rating: 0, img: '3a8a9121-d166-4259-b547-fc3b01be9b88.jpg',
-      },
-      { id: 2, name: 'Apple' },
-    ];
+    this._types = [];
+    this._brands = [];
+    this._devices = [];
+    this._selectedType = {};
+    this._selectedBrand = {};
+    this._page = 1;
+    this._totalCount = 0;
+    this._limit = 3;
     makeAutoObservable(this);
   }
 
