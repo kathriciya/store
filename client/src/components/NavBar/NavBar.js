870e228ffeb0f4815557a76bc83eed96356c5c2b
Ekaterina <Kata_Yurevna@mail.ru>
2022-09-04 21:01:05 +0700

    add navigate and correct styles

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,40 +1,57 @@
+import cn from 'classnames';
 import { observer } from 'mobx-react-lite';
 import React, { useContext } from 'react';
 import { Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { Context } from '../../context/Context';
-import { SHOP_ROUTE } from '../../utils/consts';
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from '../../utils/consts';
+import s from './NavBar.module.css';
 
 const NavBar = observer(() => {
   const { user } = useContext(Context);
+  const navigate = useNavigate();
+
+  const handleClickAdmin = () => {
+    navigate(ADMIN_ROUTE);
+  };
+
+  const handleClickLogin = () => {
+    navigate(LOGIN_ROUTE);
+  };
+
+  const logOut = () => {
+    user.setUser({});
+    user.setIsAuth(false);
+  };
 
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <NavLink style={{ color: 'white' }} to={SHOP_ROUTE}>КупиДевайс</NavLink>
+        <NavLink className={s.colorSelect} to={SHOP_ROUTE}>КупиДевайс</NavLink>
         {user.isAuth
           ? (
-            <Nav className="ml-auto" style={{ color: 'white' }}>
+            <Nav className={cn('ml-auto', s.colorSelect)}>
               <Button
+                className={s.btn}
                 variant="outline-light"
-                onClick={() => console.log('click')}
+                onClick={handleClickAdmin}
               >
                 Админ панель
               </Button>
               <Button
                 variant="outline-light"
-                onClick={() => console.log('click')}
+                onClick={() => logOut()}
               >
                 Выйти
               </Button>
             </Nav>
           )
           : (
-            <Nav className="ml-auto" style={{ color: 'white' }}>
-              <Button variant="outline-light" onClick={() => user.setIsAuth(true)}>Авторизация</Button>
+            <Nav className={cn('ml-auto', s.colorSelect)}>
+              <Button variant="outline-light" onClick={handleClickLogin}>Авторизация</Button>
             </Nav>
           )}
       </Container>
