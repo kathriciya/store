77cd21a7db4883269a149bc50e92e0ea36d0c10e
Ekaterina <Kata_Yurevna@mail.ru>
2022-09-04 21:08:20 +0700

    update ShopPage

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import { observer } from 'mobx-react-lite';
+import React, { useContext, useEffect } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import BrandBar from '../components/BrandBar/BrandBar';
+import DeviceList from '../components/DeviceList/DeviceList';
+import Pages from '../components/Pages/Pages';
+import TypeBar from '../components/TypeBar/TypeBar';
+import { Context } from '../context/Context';
+import { fetchBrands, fetchDevices, fetchTypes } from '../http/deviceAPI';
 
-function Shop() {
+const Shop = observer(() => {
+  const { device } = useContext(Context);
+
+  useEffect(() => {
+    fetchTypes().then((data) => device.setTypes(data));
+    fetchBrands().then((data) => device.setBrands(data));
+    fetchDevices(null, null, 1, 2).then((data) => {
+      device.setDevices(data.rows);
+      device.setTotalCount(data.count);
+    });
+  }, []);
+
+  useEffect(() => {
+    fetchDevices(device.selectedType.id, device.selectedBrand.id, device.page, 2).then((data) => {
+      device.setDevices(data.rows);
+      device.setTotalCount(data.count);
+    });
+  }, [device.page, device.selectedType, device.selectedBrand]);
+
   return (
-    <div>
-      SHOP
-    </div>
+    <Container>
+      <Row className="mt-2">
+        <Col md={3}>
+          <TypeBar />
+        </Col>
+        <Col md={9}>
+          <BrandBar />
+          <DeviceList />
+          <Pages />
+        </Col>
+      </Row>
+    </Container>
   );
-}
+});
 
 export default Shop;
